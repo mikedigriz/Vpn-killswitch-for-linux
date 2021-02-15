9762e5ebbbf114837acfe730c42e0ad2dc65e19e
Mihail <mikedigriz@yandex.ru>
2021-02-15 17:55:20 +0300

    Update killswitchenable.sh

--- killswitchenable.sh
+++ killswitchenable.sh
@@ -6,4 +6,4 @@ sudo ufw allow out on tun0 from any to any
 sudo ufw allow out from any to #0.0.0.0 - change your external vpn ip here
 #sudo ufw allow CUPS - printer server
 #sudo ufw allow out 9100 - print port
-
+sudo ufw enable
