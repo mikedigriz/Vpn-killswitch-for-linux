e626158ec8d97cdc0c88eaccff59a13e35de32ba
Mihail <mikedigriz@yandex.ru>
2021-02-15 17:31:34 +0300

    Update killswitchenable.sh
    add rule for reconnect to vpn

--- killswitchenable.sh
+++ killswitchenable.sh
@@ -3,6 +3,7 @@ sudo ufw reset
 sudo ufw default deny incoming
 sudo ufw default deny outgoing
 sudo ufw allow out on tun0 from any to any
-sudo ufw allow proto tcp from 192.168.1.0/24 to 170.46.27.195 comment 'allow some device'
-sudo ufw limit 1283/tcp comment 'limit some service'
-sudo ufw enable
+sudo ufw allow out from any to #0.0.0.0 - change your external vpn ip here
+#sudo ufw allow CUPS - printer server
+#sudo ufw allow out 9100 - print port
+
